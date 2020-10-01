fc9defac815f91ae3d852a29365aee5ebcb864c3
unknown <abdinajibmohamud@gmail.com>
2020-10-01 12:29:01 -0700

    restarting

--- README.md
+++
@@ -1,2 +0,0 @@
-# Property-Listings
-an React JS application that displays properties for sale and other features (desc will updated as progress is made)
--- src/utils/number.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-
-export function asCurrency(amount) {
-  if (!amount) {
-    return ''
-  }
-
-  return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
